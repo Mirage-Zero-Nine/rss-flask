bccfa6a31e67d1708e6a69835bebc62e55f6e3fc
Null <shine_rappers.0e@icloud.com>
2022-03-07 21:59:34 -0800

    fix: implement a simple rate limiter to limit query period (at most 1 time per 15 minutes)

--- router/zaobao/zaobao_router.py
+++ router/zaobao/zaobao_router.py
@@ -1,18 +1,25 @@
 import requests
+import time
 from bs4 import BeautifulSoup
 from flask import make_response
+
+from rfeed import *
 
 import constant.constants as c
 import router.zaobao.data_object as do
 import utils.generate_xml as gxml
 import utils.time_converter as tc
 
-headers = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-    'Referer': 'https://www.zaobao.com.sg/realtime',
-    'Host': 'www.zaobao.com.sg',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:97.0) Gecko/20100101 Firefox/97.0'
-}
+# todo: replace it with Redis, this is a temporary solution
+# for now using a timestamp to check refresh time, and limit to at most 15 minutes per query
+started_time = round(time.time() * 1000)
+minutes_in_millisecond_15 = 900000  # 15 minutes in millisecond
+should_query = None
+feed = Feed(
+    description=None,
+    link=None,
+    title=None
+)
 
 
 def get_news_list():
@@ -21,14 +28,15 @@ def get_news_list():
         # todo: add cache to avoid duplicate call
         data = requests.get(
             'https://www.zaobao.com.sg/realtime/world?_wrapper_format=html&page=' + str(x),
-            headers=headers
+            headers=c.zaobao_headers
         )
         soup = BeautifulSoup(data.text, 'html.parser')
         news_list = soup.find_all("div", {"class": "col col-lg-12"})  # type is bs4.element.ResultSet
 
         for news in news_list:
+            title = news.find('a').contents[0].text
             news_item = do.NewsItem()
-            news_item.title = news.find('a').contents[0].text
+            news_item.title = title
             news_item.link = c.zaobao_story_prefix + news.find('a')['href']
             output_news_list.append(news_item)
 
@@ -37,6 +45,7 @@ def get_news_list():
 
 def get_individual_news_content(news_list):
     for item in news_list:
+        # add to cache to avoid duplication
         soup = get_link_content(item.link)
         post_list = soup.find_all(
             'div',
@@ -53,10 +62,10 @@ def get_individual_news_content(news_list):
         news_text = ''
         for t in time_list:
             item.created_time = t.text.split('/')[1].strip()
+
         for e in title_list:
             item.author = e.find_all('a')[0].text.strip()
         for post in post_list:
-            # print(str(post))
 
             for text in post.find_all('p'):
                 news_text += str(text)
@@ -64,9 +73,17 @@ def get_individual_news_content(news_list):
 
 
 def get_link_content(link):
-    data = requests.get(link, headers=headers)
+    data = requests.get(link, headers=c.zaobao_headers)
     soup = BeautifulSoup(data.text, 'html.parser')
     return soup
+
+
+def get_rss_xml():
+    global feed
+    if check_if_should_query() is True:
+        feed = generate_news_rss_feed()
+    else:
+        return feed
 
 
 def generate_news_rss_feed():
@@ -96,6 +113,23 @@ def generate_news_rss_feed():
     response = make_response(feed)
     response.headers.set('Content-Type', 'application/rss+xml')
     return response
+
+
+def check_if_should_query():
+    """
+    Limit query to at most 1 time in 15 minutes.
+    Todo: refactor this implementation by using redis to both dedup and limit query speed.
+    :return: if service should query now
+    """
+    global should_query
+    global started_time
+    # if it's the first query, or the last query happened more than 15 minutes, then query again
+    if should_query is None or round(time.time() * 1000) - started_time > minutes_in_millisecond_15:
+        should_query = False
+        started_time = round(time.time() * 1000)
+        return True
+
+    return False
 
 
 if __name__ == '__main__':
