1f688995791ff39e2d5a395e0d5b66c465824e09
Null <shine_rappers.0e@icloud.com>
2022-04-11 21:09:56 -0700

    style: minor revision

--- router/dayone/dayone_router.py
+++ router/dayone/dayone_router.py
@@ -56,7 +56,7 @@ def get_individual_article(entry_list):
             # sample metadata: December 31, 2020 by The Day One Team
             split_metadata = metadata_list[0].text.split(" by ")
             post.created_time = tc.convert_time_with_pattern(split_metadata[0].strip(),
-                                                              c.dayone_time_convert_pattern)
+                                                             c.dayone_time_convert_pattern)
             post.author = split_metadata[1]
             for description in description_list:
                 for text in description.find_all('p'):
