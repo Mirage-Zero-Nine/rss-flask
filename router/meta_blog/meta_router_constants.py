08573b0be0d1bff534d22792280c1c4000b3264d
Null <shine_rappers.0e@icloud.com>
2023-09-25 10:59:11 -0700

    router: minor fix

--- router/meta_blog/meta_router_constants.py
+++ router/meta_blog/meta_router_constants.py
@@ -3,6 +3,6 @@ meta_blog_rss_link = "https://code.facebook.com/posts/rss/"
 meta_blog_link = "https://engineering.fb.com/"
 meta_blog_description = "Meta believes in building community through open source technology. Explore our latest projects in Artificial Intelligence, Data Infrastructure, Development Tools, Front End, Languages, Platforms, Security, Virtual Reality, and more. "
 meta_blog_key = "/meta/blog"
-meta_blog_period = 20 * 60 * 1000  # 20 minutes
+meta_blog_period = 30 * 60 * 1000  # 30 minutes
 meta_ai_blog_prefix = "https://ai.meta"
 meta_blog_prefix = "https://www.meta"
